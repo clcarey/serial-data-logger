2ebea7aa90033f6e14aa39c1fe24e42fb5c34cfe
Chuck <clcarey@mtu.edu>
2025-04-11 14:04:50 -0400

    Added UTC to file management

--- fileManagement.py
+++ fileManagement.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import time
 
 class saveFile():
     def __init__(self):
@@ -22,14 +23,18 @@ class saveFile():
             self.full_fileName = self.filename + str(ind) + self.file_ext
             ind = ind + 1
         self.data_expected = len(column_names)
+        writecolumn = [str("UTC")]
+        writecolumn.extend(column_names)
         with open(self.full_fileName,'w',newline='') as logFile:
             logWriter = csv.writer(logFile)
-            logWriter.writerow(column_names)
+            logWriter.writerow(writecolumn)
 
     def write_row(self, data):
         with open(self.full_fileName,'a',newline='') as logFile:
             logWriter = csv.writer(logFile)
-            logWriter.writerow(data)
+            writedata = [str(time.time())]
+            writedata.extend(data)
+            logWriter.writerow(writedata)
     
     def check_file(self):
         return os.path.isfile(self.full_fileName)
